7ef3b5293be391aedc5a4ef7d89e6039649fabdb
Tomek Wałkuski <ja@jestem.tw>
2012-09-07 20:26:57 +0200

    Don't provide links to problems

--- haskell/Problem_1.hs
+++ haskell/Problem_1.hs
@@ -4,8 +4,6 @@
 -- we get 3, 5, 6 and 9. The sum of these multiples is 23.
 --
 -- Find the sum of all the multiples of 3 or 5 below 1000.
---
--- http://projecteuler.net/problem=1
 
 module Problem_1 where
 
--- haskell/Problem_2.hs
+++ haskell/Problem_2.hs
@@ -7,8 +7,6 @@
 --
 -- By considering the terms in the Fibonacci sequence whose values do not exceed
 -- four million, find the sum of the even-valued terms.
---
--- http://projecteuler.net/problem=2
 
 module Problem_2 where
 
