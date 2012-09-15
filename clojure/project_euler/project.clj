2820cc60a5017e788a82d393075780d9aabb31e9
Tomek Wałkuski <ja@jestem.tw>
2012-09-15 21:11:02 +0200

    Implement Problem 1 in Clojure

---
+++ clojure/project_euler/project.clj
@@ -0,0 +1,3 @@
+(defproject project_euler "1.0.0-SNAPSHOT"
+  :description "Project Euler in Clojure"
+  :dependencies [[org.clojure/clojure "1.4.0"]])
---
+++ clojure/project_euler/test/project_euler/test/problem_1.clj
@@ -0,0 +1,6 @@
+(ns project_euler.test.problem_1
+  (:use [project_euler.problem_1])
+  (:use [clojure.test]))
+
+(deftest test-sum-of-multiplies
+  (is (= 23 (sum-of-multiplies 9))))
